ef36e76f664916cc762766c1d2048deef711fec1
AbrahamGCh <1823110013@untels.edu.pe>
2024-07-22 14:53:34 -0500

    Garcia Examen Final - Parte 1 -Site

---
+++ efi/miapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def saludo(request):
+    return render(request, 'saludo.html')
+
+def integrantes(request):
+    return render(request, 'integrantes.html')
